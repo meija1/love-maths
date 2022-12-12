8f1a4c351f3671bfb2140ef38f879bd51b687839
meija1 <meija911@gmail.com>
2022-12-12 16:37:24 +0000

    Add operands and operator to question addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,26 @@ document.addEventListener("DOMContentLoaded", function(){
             }else{
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
-})
 
-function runGame(){
+    runGame("addition");
+})
+/**
+ * Main game
+ */
+function runGame(gameType){
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gametype}.Aborting`
+    }
 
 }
 
@@ -33,8 +47,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestin(){
