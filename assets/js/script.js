41332f266079c7808389ca0603e6bcb4c3b71ee7
meija1 <meija911@gmail.com>
2022-12-13 15:26:17 +0000

    Add increment score and wrong answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,8 +38,10 @@ function checkAnswer(){
 
     if(isCorrect){
         alert("Correct");
+        incrementScore();
     }else{
         alert(`You answered: ${userAnswer}. The correct answer is ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -60,11 +62,13 @@ function calculateCorrectAnswer(){
 }
 
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
