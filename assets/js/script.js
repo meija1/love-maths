8fd18d4c631caff007fcac8fe2526be3ee8a912f
meija1 <meija911@gmail.com>
2022-12-14 15:42:43 +0000

    Add subtraction code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,9 @@ function runGame(gameType){
     if(gameType === "addition"){
         displayAdditionQuestion(num1, num2);
     }else if(gameType === "multiply"){
-        displayMultiplyQuestion(num1, num2)
+        displayMultiplyQuestion(num1, num2);
+    }else if(gameType === "division"){
+        displaySubtractQuestin(num1, num2);
     }else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gametype}.Aborting`
@@ -58,6 +60,8 @@ function calculateCorrectAnswer(){
         return [operand1 + operand2, "addition"];
     }else if (operator === "X"){
         return[operand1 * operand2, "multiply"];
+    }else if (operator === "-"){
+        return[operand1 - operand2, "division"];
     }else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting`;
@@ -82,7 +86,9 @@ function displayAdditionQuestion(operand1, operand2){
 }
 
 function displaySubtractQuestin(){
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2){
